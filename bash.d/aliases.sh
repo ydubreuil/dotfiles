c9974a61dd2ed5658c5e2c90e70aa43d369108ae
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2014-07-31 17:33:06 +0200

    reformat aliases.sh + psme()

--- bash.d/aliases.sh
+++ bash.d/aliases.sh
@@ -1,10 +1,12 @@
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+  test -r ~/.dircolors && \
+    eval "$(dircolors -b ~/.dircolors)" || \
+    eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
 
 # some more ls aliases
@@ -17,47 +19,52 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 init_dot_aliases() {
-    local slashes
-    slashes="../.."
+  local slashes
+  slashes="../.."
 
-    for c in $(seq 3 10)
-    do
-        slashes="../${slashes}"
-        alias .$c="cd ${slashes}"
-    done
+  for c in $(seq 3 10)
+  do
+    slashes="../${slashes}"
+    alias .$c="cd ${slashes}"
+  done
 }; init_dot_aliases
 
 # generate new password
 newpass() {
-    local length; length=$1; [ -z "$length" ] && length=12
-
-    apg -a 1 -m $length -x $length -n 1 -E \'\"
+  local length; length=$1; [ -z "$length" ] && length=12
+  apg -a 1 -m $length -x $length -n 1 -E \'\"
 }
 
 
 ssh() {
-   local args=( "${@}" )
-   local i x
-   for (( i=0; i<${#args[@]}; i++ )) ; do
-     x=${args[i]}
-     [[ -z ${x%%-[bcDeFIiLlmOopRSWw]} ]] && (( i++ )) && continue
-     [[ -z ${x%%-*} ]] && continue
-     x=${x#http://}
-     x=${x#https://}
-     x=${x/@http:\/\//@}
-     x=${x/@https:\/\//@}
-     args[i]=${x}
-     break
-   done
-   command ssh "${args[@]}"
+  local args=( "${@}" )
+  local i x
+  for (( i=0; i<${#args[@]}; i++ )) ; do
+    x=${args[i]}
+    [[ -z ${x%%-[bcDeFIiLlmOopRSWw]} ]] && (( i++ )) && continue
+    [[ -z ${x%%-*} ]] && continue
+    x=${x#http://}
+    x=${x#https://}
+    x=${x/@http:\/\//@}
+    x=${x/@https:\/\//@}
+    args[i]=${x}
+    break
+  done
+  command ssh "${args[@]}"
 }
 
 tabandstrip() {
-    local args=( "${@}" )
-    local i
-    for (( i=0; i<${#args[@]}; i++ )) ; do
-        local filename=${args[i]}
-        unexpand -t 4 -a "$filename" | sed -E "s/[[:space:]]$//" | sponge "$filename"
-    done
+  local args=( "${@}" )
+  local i
+  for (( i=0; i<${#args[@]}; i++ )) ; do
+    local filename=${args[i]}
+    unexpand -t 4 -a "$filename" | \
+      sed -E "s/[[:space:]]$//" | \
+      sponge "$filename"
+  done
+}
+
+psme() {
+  ps awwf -U $(id -u)
 }
 
